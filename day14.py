d18e6aa4d87934b5e38e94c614a70c758377b202
Vancha <tjipkevanderheide90@gmail.com>
2023-12-15 16:53:19 +0100

    Update day14.py

--- day14.py
+++ day14.py
@@ -9,29 +9,34 @@ O.#..O.#.#
 #....###..
 #OO..#...."""
 
+with open('input_data.txt','r') as f:
+    input_data = f.read()
+
 
 class platform:
     def __init__(self, input_data):
         split = input_data.strip().split("\n")
         self.grid = [list(i.strip()) for i in split]
-      
-    #tilt the grid left, and then back to neutral
+
     def tilt_left(self):
         work_to_do = True
 
         while work_to_do:
             work_to_do = False
             for row_idx, row in enumerate(self.grid):
-                for idx, spot in enumerate(''.join(row)):
-                    row = ''.join(row)
+                for idx, spot in enumerate("".join(row)):
+                    row = "".join(row)
                     if idx > 0 and spot == "O" and row[idx - 1] == ".":
                         work_to_do = True
-                        self.grid[row_idx] = row[0 : idx - 1] + row[idx] + "." + row[idx + 1 :]
-                        
-    #rotate the entire grid right
+                        self.grid[row_idx] = (
+                            row[0 : idx - 1] + row[idx] + "." + row[idx + 1 :]
+                        )
+
     def rotate_right(self):
-        #i stole this line... it's pretty cool though
-        self.grid = list(zip(*self.grid[::-1])) #[''.join(list(row)) for row in zip(*self.grid)]
+        # i stole this line... it's pretty cool though
+        self.grid = list(
+            zip(*self.grid[::-1])
+        )  # [''.join(list(row)) for row in zip(*self.grid)]
 
     def tilt_north(self):
         self.rotate_right()  # north faces east
@@ -43,15 +48,15 @@ class platform:
     def print(self):
         print()
         for row in self.grid:
-            print(''.join(row))
+            print("".join(row))
         print()
-    
+
     def get_score(self):
         score = 0
         for row_index, row in enumerate(self.grid):
-            for character in ''.join(row):
-                if character == 'O':
-                    score  += len(self.grid) - row_index
+            for character in "".join(row):
+                if character == "O":
+                    score += len(self.grid) - row_index
         return score
 
 
