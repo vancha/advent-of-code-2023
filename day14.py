1c764b4798db4b50b5fb694be4855ff3d4400674
Vancha <tjipkevanderheide90@gmail.com>
2023-12-15 16:49:43 +0100

    Create day14.py

---
+++ day14.py
@@ -0,0 +1,61 @@
+input_data = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+
+
+class platform:
+    def __init__(self, input_data):
+        split = input_data.strip().split("\n")
+        self.grid = [list(i.strip()) for i in split]
+      
+    #tilt the grid left, and then back to neutral
+    def tilt_left(self):
+        work_to_do = True
+
+        while work_to_do:
+            work_to_do = False
+            for row_idx, row in enumerate(self.grid):
+                for idx, spot in enumerate(''.join(row)):
+                    row = ''.join(row)
+                    if idx > 0 and spot == "O" and row[idx - 1] == ".":
+                        work_to_do = True
+                        self.grid[row_idx] = row[0 : idx - 1] + row[idx] + "." + row[idx + 1 :]
+                        
+    #rotate the entire grid right
+    def rotate_right(self):
+        #i stole this line... it's pretty cool though
+        self.grid = list(zip(*self.grid[::-1])) #[''.join(list(row)) for row in zip(*self.grid)]
+
+    def tilt_north(self):
+        self.rotate_right()  # north faces east
+        self.rotate_right()  # north faces south
+        self.rotate_right()  # north faces west
+        self.tilt_left()  # tilt when north points west
+        self.rotate_right()  # make north face up again
+
+    def print(self):
+        print()
+        for row in self.grid:
+            print(''.join(row))
+        print()
+    
+    def get_score(self):
+        score = 0
+        for row_index, row in enumerate(self.grid):
+            for character in ''.join(row):
+                if character == 'O':
+                    score  += len(self.grid) - row_index
+        return score
+
+
+p = platform(input_data)
+p.tilt_north()
+score = p.get_score()
+print(score)
